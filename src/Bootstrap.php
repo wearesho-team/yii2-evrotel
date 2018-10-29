b21de7a36438b744251a24160b6a35dfa7ff43e5
Alexander Letnikow <reclamme@gmail.com>
2018-10-29 18:07:32 +0200

    Add adding package alias in common boostrap

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -46,6 +46,8 @@ class Bootstrap extends base\BaseObject implements base\BootstrapInterface
      */
     public function bootstrap($app): void
     {
+        \Yii::setAlias('Wearesho/Evrotel/Yii', '@vendor/wearesho-team/yii2-evrotel/src');
+
         if (!is_null($this->web) && $app instanceof web\Application) {
             /** @var Evrotel\Yii\Web\Bootstrap $bootstrap */
             $bootstrap = di\Instance::ensure($this->web, Evrotel\Yii\Web\Bootstrap::class);
