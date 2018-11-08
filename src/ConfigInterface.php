0051676e5173e583dbef7b9209ab16508024e81c
Alexander Letnikow <reclamme@gmail.com>
2018-11-08 17:37:13 +0200

    Extend Config interface and implementations from base Config

--- src/ConfigInterface.php
+++ src/ConfigInterface.php
@@ -2,11 +2,13 @@
 
 namespace Wearesho\Evrotel\Yii;
 
+use Wearesho\Evrotel;
+
 /**
  * Interface ConfigInterface
  * @package Wearesho\Evrotel\Yii
  */
-interface ConfigInterface
+interface ConfigInterface extends Evrotel\ConfigInterface
 {
     public const DEFAULT_CHANNELS = 5;
     public const DEFAULT_JOB_INTERVAL = 1;
