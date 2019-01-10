7c78d9b99cb47fc1536ae767656afdb4d082bcc8
Alexander Letnikow <reclamme@gmail.com>
2019-01-10 12:52:56 +0200

    Fix breaking check process on atomic save error

--- src/Console/Action/Check.php
+++ src/Console/Action/Check.php
@@ -6,10 +6,11 @@ namespace Wearesho\Evrotel\Yii\Console\Action;
 
 use Carbon\Carbon;
 use Horat1us\Yii\Exceptions\ModelException;
+use Horat1us\Yii\Interfaces\ModelExceptionInterface;
 use Wearesho\Evrotel;
-use yii\db;
 use yii\di;
 use yii\base;
+use yii\helpers\Console;
 
 /**
  * Class Check
@@ -41,33 +42,35 @@ class Check extends base\Action
     /**
      * @param string|null $date
      * @throws \GuzzleHttp\Exception\GuzzleException
-     * @throws \Horat1us\Yii\Interfaces\ModelExceptionInterface
      */
     public function run(string $date = null)
     {
         $date = is_string($date) ? Carbon::parse($date) : Carbon::now();
-        $calls = $this->client->getCalls((bool)$this->isAuto);
+        $calls = $this->client->getCalls((bool)$this->isAuto, $date);
         /** @var Evrotel\Yii\Call[] $records */
         $records = [];
         /** @var Evrotel\Statistics\Call $call */
         foreach ($calls as $call) {
-
             $this->controller->stdout($call->getId() . "\t");
             $record = Evrotel\Yii\Call::from($call);
             if ($record->isDuplicate()) {
-                $this->controller->stdout("Skip\n", \yii\helpers\Console::FG_YELLOW);
+                $this->controller->stdout("Skip\n", Console::FG_YELLOW);
                 continue;
             }
-            /** @noinspection PhpUnhandledExceptionInspection */
-            Evrotel\Yii\Call::getDb()->transaction(function () use (
-                $records,
-                $record
-            ): db\ActiveRecordInterface {
-                return ModelException::saveOrThrow($record);
-            });
+            try {
+                /** @noinspection PhpUnhandledExceptionInspection */
+                Evrotel\Yii\Call::getDb()->transaction(function () use (
+                    $record
+                ): void {
+                    ModelException::saveOrThrow($record);
+                });
+            } /** @noinspection PhpRedundantCatchClauseInspection */ catch (ModelExceptionInterface $exception) {
+                $this->controller->stdout($exception->getMessage() . "\n", Console::FG_RED);
+                return;
+            }
 
-            $records[] = ModelException::saveOrThrow($record);
-            $this->controller->stdout("Save\n", \yii\helpers\Console::FG_GREEN);
+            $records[] = $record;
+            $this->controller->stdout("Save\n", Console::FG_GREEN);
         }
         $this->controller->stdout("Saved " . count($records) . " calls\n");
     }
